adf1a9abde86512480a5d3a2c5adaa38fd9e3101
stephen s <lunar.city.team@gmail.com>
2014-09-26 16:58:08 -0400

    Modules ex

---
+++ HardPathon/ex11.py
@@ -0,0 +1,9 @@
+#print("How old are you?")
+age = input("How old are you?")
+#print("How tall are you?")
+height = input("How tall are you?")
+#print("How much do you weight?")
+weight = input("How much do you weight?")
+
+print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
+
