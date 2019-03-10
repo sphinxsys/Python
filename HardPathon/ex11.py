ec6c3dab1b2621eae08b422791b685f464f6967c
sys <syone2011@gmail.com>
2019-03-10 22:28:49 +0000

    Remove HardPython project

--- HardPathon/ex11.py
+++
@@ -1,9 +0,0 @@
-#print("How old are you?")
-age = input("How old are you?")
-#print("How tall are you?")
-height = input("How tall are you?")
-#print("How much do you weight?")
-weight = input("How much do you weight?")
-
-print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
-
