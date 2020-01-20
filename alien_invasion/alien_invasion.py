70685e25fcbeb68dbede7b5efc6544fabb00d94a
sys <syone2011@gmail.com>
2020-01-20 14:56:03 -0500

    add LEFT/RIGHT key events

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
         while True:
             """ Start the main loop for the game. """
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             # Make the most recently draw screen visible
@@ -34,6 +35,17 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        # Mark the moving_right flag as TRUE.
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
 
     def _update_screen(self):
         """ Update images on the screen, and flip to the new screen. """
