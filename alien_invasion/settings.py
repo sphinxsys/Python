13d86e0ed9c88369739d21d202087fe5825abed1
sys <syone2011@gmail.com>
2020-01-20 17:45:31 -0500

    Add bullet class

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,3 +7,13 @@ class Settings():
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+
+        # ship settings
+        # movement speed
+        self.ship_speed = 0.5
+
+        # Bullet settings
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -8,6 +8,7 @@ class Ship():
         """ Initialize the ship and start its starting position. """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load space ship image
         path = os.path.join("images", "ship.bmp")
@@ -17,16 +18,23 @@ class Ship():
         # Start a new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """ Update the ship's position based on the movement flag. """
-        if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+        # Update the ship's x instead of the rect's x directly
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x
+        self.rect.x = self.x
     
     def blitme(self):
         """ Draw the sapce ship at its current postion. """
